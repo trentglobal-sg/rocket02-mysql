78250be4d24459c489a28b4e25b08e920170e95d
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-09-12 05:59:24 +0000

    Added U and D

--- 04-crud/index.js
+++ 04-crud/index.js
@@ -13,6 +13,11 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// set up express to use web forms (or forms that submitted directly to the server)
+app.use(express.urlencoded({
+    extended: true
+}))
+
 // create a connection to the database
 const dbConfig = {
     host: process.env.DB_HOST,
@@ -36,8 +41,95 @@ app.get('/', function(req,res){
 })
 
 app.get('/food_entries', async function(req,res){
+    // array destructuring
+    // see: https://onecompiler.com/javascript/452z3c982
+    // dbConnection.execute returns more than one element in an array as the return
+    // element 0 is the result from the database
+    // element 1 are meta-data
     const [rows] = await dbConnection.execute("SELECT * FROM food_entries");
-   res.send(rows);
+
+    // we could do:
+    // const results = await dbConnection.execute("SELECT * FROM food_entries");
+    // const rows = results[0];
+   res.render("food_entries", {
+        results: rows
+   });
+})
+
+// render (i.e show a form) to the user
+app.get('/create_food_entry',  function(req,res){
+    res.render('create_food_entry');
+})
+
+app.post('/create_food_entry', async function(req,res){
+    console.log(req.body)
+    const sql = `insert into food_entries (dateTime, foodName, calories, meal, tags, servingSize, unit)
+            VALUES ( NOW(), ?, ?, ?, ?, ?, ?);`
+    await dbConnection.execute(sql, [
+        req.body.foodName,
+        req.body.calories,
+        req.body.meal,
+        JSON.stringify(req.body.tags),
+        req.body.servingSize,
+        req.body.unit
+
+    ]);
+   res.redirect('/food_entries')
+});
+
+app.get('/confirm_delete_food_entry/:id', async function(req,res){
+    const foodEntryId = req.params.id;
+    // use prepared query (i.e paramterized query)
+    const [rows] = await dbConnection.execute(
+        "SELECT foodName, calories, dateTime FROM food_entries WHERE id = ?", [foodEntryId]
+    );
+    // dbConnection.execute with SELECT will always return an array, so if we want the first found result
+    // we must reference index 0
+    const foodEntry = rows[0];
+    res.render('confirm_delete_food_entry', {
+        foodEntry
+    })
+})
+
+app.post('/confirm_delete_food_entry/:id', async function(req,res){
+    const foodEntryId = req.params.id;
+    const sql = "DELETE FROM food_entries WHERE id = ?";
+    await dbConnection.execute(sql, [foodEntryId]);
+    res.redirect('/food_entries');
+})
+
+app.get('/edit_food_entry/:id', async function(req,res){
+    const foodEntryId = req.params.id;
+    const sql = "SELECT * FROM food_entries WHERE id = ?";
+    const [rows] = await dbConnection.execute(sql, [foodEntryId]);
+    const foodEntry = rows[0];
+    res.render('edit_food_entry', {
+        foodEntry
+    })
+})
+
+app.post('/edit_food_entry/:id', async function(req,res){
+    const foodEntryId = req.params.id;
+    const sql = `UPDATE food_entries SET 
+                    foodName = ?,
+                    calories = ?,
+                    meal = ?,
+                    tags = ?,
+                    servingSize = ?,
+                    unit = ?
+                WHERE id = ?
+    `
+    await dbConnection.execute(sql, [
+        req.body.foodName,
+        req.body.calories,
+        req.body.meal,
+        JSON.stringify(req.body.tags),
+        req.body.servingSize,
+        req.body.unit,
+        foodEntryId
+    ]);
+
+    res.redirect('/food_entries');
 })
 
 app.listen(port, function(){
---
+++ 04-crud/mock_queries.sql
@@ -0,0 +1,2 @@
+insert into food_entries (dateTime, foodName, calories, meal, tags, servingSize, unit)
+    VALUES ( NOW(), "Ramen", 800, "Lunch", '["healthy"]', 1, "bowl");
